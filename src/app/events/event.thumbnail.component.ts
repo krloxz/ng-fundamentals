d9cacec369c07894897d01535d230dcd56db9ad2
Carlos Gomez <isc.carlos.gomez@gmail.com>
2019-05-25 11:37:07 -0400

    Add a child component

---
+++ src/app/events/event.thumbnail.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'event-thumbnail',
+  template: `
+    <div>
+      <h1>Upcoming Angular Events</h1>
+      <hr />
+      <div class="well hoverwell thumbnail">
+        <h2>{{event.name}}</h2>
+        <div>Date: {{event.date}}</div>
+        <div>Time: {{event.time}}</div>
+        <div>Price: \${{event.price}}</div>
+        <div>
+          <span>Location: {{event.location.address}}</span>
+          <span>&nbsp;</span>
+          <span>{{event.location.city}}, {{event.location.country}}</span>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: [`
+   div .well {
+      color: #bbb
+    }
+  `]
+})
+export class EventTumbnailComponent {
+  @Input() event: any;
+}
