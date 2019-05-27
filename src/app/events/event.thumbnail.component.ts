f8302c5272267970a8c3b9777ed351d35efdf281
Carlos Gomez <isc.carlos.gomez@gmail.com>
2019-05-27 16:31:02 -0400

    Display multiple events using ngFor

--- src/app/events/event.thumbnail.component.ts
+++ src/app/events/event.thumbnail.component.ts
@@ -3,26 +3,22 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'event-thumbnail',
   template: `
-    <div>
-      <h1>Upcoming Angular Events</h1>
-      <hr />
-      <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-          <span>Location: {{event.location.address}}</span>
-          <span>&nbsp;</span>
-          <span>{{event.location.city}}, {{event.location.country}}</span>
-        </div>
+    <div class="well hoverwell thumbnail">
+      <h2>{{event.name}}</h2>
+      <div>Date: {{event.date}}</div>
+      <div>Time: {{event.time}}</div>
+      <div>Price: \${{event.price}}</div>
+      <div>
+        <span>Location: {{event.location.address}}</span>
+        <span>&nbsp;</span>
+        <span>{{event.location.city}}, {{event.location.country}}</span>
       </div>
     </div>
   `,
   styles: [`
-   div .well {
-      color: #bbb
-    }
+    .thumbnail { min-height: 210px; }
+    .pad-left { margin-left: 10px; }
+    .well div { color: #bbb; }
   `]
 })
 export class EventTumbnailComponent {
