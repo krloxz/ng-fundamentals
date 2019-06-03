82e89cbe4d0eac9c6dafcee34acab148b04ede2e
Carlos Gomez <isc.carlos.gomez@gmail.com>
2019-06-03 18:34:38 -0400

    Demo how to style active routes

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-navbar',
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
+  styles: [`
+    li > a.active { color: #F97924; }
+  `]
 })
 export class NavBarComponent {
 
