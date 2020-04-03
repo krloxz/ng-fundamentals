a28e7fd4a4199dc3e75efa7686267a031dd4908d
Carlos Gomez <isc.carlos.gomez@gmail.com>
2020-04-03 12:47:09 -0400

    Wire up the events dropdown

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../user/auth.service';
 import { User } from '../user/user.model';
-import { Session } from '../events/shared/event.model';
+import { Session, Event } from '../events/shared/event.model';
 import { EventService } from '../events/shared/event.service';
 
 @Component({
@@ -11,13 +11,20 @@ import { EventService } from '../events/shared/event.service';
     li > a.active { color: #F97924; }
   `]
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
 
   currentUser: User;
   searchTerm = '';
   foundSessions: Session[];
+  events: Event[];
 
   constructor(private authService: AuthService, private eventService: EventService) {}
+
+  ngOnInit() {
+    this.eventService.getEvents().subscribe(events => {
+      this.events = events;
+    });
+  }
 
   isAuthenticated(): boolean {
     const isAuthenticated = this.authService.isAuthenticated();
